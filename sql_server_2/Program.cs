6ce71ff43a4c5a37ee182a8ceba926be3a0a9633
Mohithbellamkonda <88969197+Mohithbellamkonda@users.noreply.github.com>
2022-11-09 08:24:50 +0530

    Add files via upload

---
+++ sql_server_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.SqlClient;
+namespace sql_server_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connection = "Data Source=DESKTOP-TH8OA6H;Initial Catalog=mohith;trusted_connection=true";
+            SqlConnection conn = new SqlConnection(connection);
+            conn.Open();
+            Console.WriteLine("Enter the threshold_amount");
+            ulong balance = Convert.ToUInt64(Console.ReadLine());
+            SqlCommand cmd = new SqlCommand("select * from Show(@Balance)", conn);
+            cmd.Parameters.Add("@Balance", System.Data.SqlDbType.BigInt).Value = balance;
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.Write(reader[0] + "  ");
+                Console.Write(reader[1] + "  ");
+                Console.Write(reader[2] + "  ");
+                Console.Write(reader[3] + "  ");
+                Console.Write(reader[4] + "  ");
+                Console.Write(reader[5] + "  ");
+
+                Console.WriteLine();
+            }
+            Console.WriteLine("Execution successfully completed!!");
+            reader.Close();
+            conn.Close();
+            
+        }
+        
+    }
+}
